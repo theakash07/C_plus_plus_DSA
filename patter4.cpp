800518c5d8ae89d4aa6ecab981df5aa91b88848a
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-13 04:21:29 +0530

    Read Extended Description
    Reverse conitunous patter 2D

---
+++ patter4.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"Enter number of rows you want:";
+    cin>>n;
+    cout<<'\n';
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<n-j+1;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
