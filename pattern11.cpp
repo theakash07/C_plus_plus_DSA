7aacbe6c198301a45d9affe95e1069a8de75dcc4
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-13 05:02:52 +0530

    Read Extended Description
    A	A	A	A	A
    B	B	B	B	B
    C	C	C	C	C
    D	D	D	D	D
    E	E	E	E	E

---
+++ pattern11.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"Enter number of rows you want:";
+    cin>>n;
+    cout<<'\n';
+    for(int i=1;i<=n;i++)
+    {
+        
+        for(int j=1;j<=n;j++)
+        {
+            char ch='A'+i-1;
+          cout<<"\t"<<ch;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
