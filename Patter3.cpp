9cd27792c456c85fa391f6bb4f07831af57f0598
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-13 04:16:57 +0530

    Read Extended Desctiption
    program to pring continous number patter

---
+++ Patter3.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"Enter number of rows you want";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
