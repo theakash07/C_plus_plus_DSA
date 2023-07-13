03120a568cf417f5aee745cca37af29d78e5b0ee
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-13 05:34:39 +0530

    Read Extended Desctiption
    E	F
    D	EE	F
    C	DD	EE	F
    B	CC	DD	EE	F
    A	BB	CC	DD	EE	F

---
+++ pattern16.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"Enter number of rows you want:";
+    cin>>n;
+    cout<<'\n';
+    int count=1;
+    for(int i=1;i<=n;i++)
+    {
+        char ch='A'+n-i;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<ch<<"";
+            ch++;
+            cout<<"\t"<<ch;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
