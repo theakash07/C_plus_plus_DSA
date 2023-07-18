4d880d7c88de2a8c33299c238d5c4457fa6c3d82
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2023-07-18 16:10:55 +0530

    Update swappingalternate.cpp

--- swappingalternate.cpp
+++ swappingalternate.cpp
@@ -27,3 +27,38 @@ int main()
     
     return 0;
 }
+//Approach 2
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5,7};
+//     int start=0;
+//     int next=0+1;
+//     int i = 0;
+    
+//     while (next < 6)
+//     {
+        
+//         // Check if the 'next' index is within the array bounds
+//             // Swap elements at positions 'start' and 'next'
+//             swap(arr[start], arr[next]);
+        
+//         // Move 'start' and 'next' to the next pair of elements
+//         start = start + 2;
+//         next = next + 2;
+        
+//     }
+    
+//     // Print the modified array
+//     for (int j = 0; j < 6; j++)
+//     {
+//         cout << arr[j] << " ";
+//     }
+//     cout << endl;
+    
+//     return 0;
+// }
+
