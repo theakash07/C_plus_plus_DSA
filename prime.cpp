57ce2f973f5949b89d8cf3217b17a08fd81bfd34
Akash kumar (sky) <theakash.education07@gmail.com>
2023-09-21 01:45:12 -0700

    Added new approach

--- prime.cpp
+++ prime.cpp
@@ -62,3 +62,30 @@ int main() {
 
     return 0;
 }
+//approach 2
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter Limit";
+    cin>>n;
+    for(int i=2;i<=n;i++)
+    {
+        int count=0;
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+            {
+                cout<<i<<endl;
+            }
+    }
+    
+    return 0;
+    
+}
